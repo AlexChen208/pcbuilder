c57b79a8810cc0d4984bff655ac139001195b319
bonnie430 <bonniecai97@gmail.com>
2022-07-18 14:48:09 -0700

    add a detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('cases/', views.cases_index, name='index'),
+    path('cases/<int:case_id>/',views.cases_detail,name='detail'),
     path('account/signup/', views.signup, name='signup'),
+   
    
     
 ]
