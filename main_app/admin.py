ae0fe942c66a9924f94ca93de37815ff66100c28
AlexChen208 <alex.chencs207@gmail.com>
2022-07-19 12:31:31 -0700

    Add Photo's model/view/url

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Case,Comment,Component
+from .models import Case,Comment,Component,Photo
 # Register your models here.
 admin.site.register(Case)
 admin.site.register(Comment)
 admin.site.register(Component)
+admin.site.register(Photo)
